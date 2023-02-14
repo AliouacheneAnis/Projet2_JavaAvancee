b6212715b39fc1986f31410fbd0f80dea66073c2
AliouacheneAnis <eaalioua01@monccnb.ca>
2023-02-14 10:44:03 -0400

    Ajout abtract class et final

--- projet/Chambre/App.java
+++ projet/Chambre/App.java
@@ -11,82 +11,79 @@ public class App {
         Bedroom myBedroom = new Bedroom("Bedroom1");
         garage myGarage = new garage("Garage1");
         Kitchen myKitchen = new Kitchen("Kitchen 1");
-        myBedroom.ChangerEtatClimatiseur();
-        myGarage.opendoor();
-
+        Afficher f = new Afficher();
 
         int choix = 0, choixRep = 0;
 
         Scanner sc = new Scanner(System.in);
 
-        while (choixRep != 5) {
+        while (choixRep != 4) {
             Scanner s = new Scanner(System.in);
             // Affichage Menu
-            Menu.AfficherMenu();
+            f.AfficherMenu();
             choixRep = sc.nextInt(); // lecture de choix de l'utilisateur
-            
             switch (choixRep) {
                 case 1:
-                    Menu.MenuBedroom();
+                    f.MenuBedroom();
                     choix = s.nextInt();
 
-                        switch (choix) {
-                            case 1:
-                                myBedroom.AllumerLumiere();
-                                break;
+                    switch (choix) {
+                        case 1:
+                            myBedroom.AllumerLumiere();
+                            break;
 
-                            case 2:
-                                myBedroom.ChangerEtatClimatiseur();
-                                break;
+                        case 2:
+                            myBedroom.ChangerEtatClimatiseur();
+                            break;
 
-                            default:
-                                System.out.println("Error : Merci de Saisir un chiffre soit 1 ou 2");
-                                choix = s.nextInt();
-                                break;
-                        }
+                        default:
+                            System.out.println("Error : Merci de Saisir un chiffre soit 1 ou 2");
+                            choix = s.nextInt();
+                            break;
+                    }
                     break;
 
                 case 2:
 
-                    Menu.MenuCuisine();
+                    f.MenuCuisine();
                     choix = s.nextInt();
-                        switch (choix) {
-                            case 1:
-                                 myKitchen.AllumerFour();
-                                break;
+                    switch (choix) {
+                        case 1:
+                            myKitchen.AllumerFour();
+                            break;
 
-                            case 2:
-                                myKitchen.closedoor();
-                                break;
+                        case 2:
+                            myKitchen.closedoor();
+                            break;
 
-                            default:
+                        default:
 
-                                System.out.println("Error : Merci de Saisir un chiffre soit 1 ou 2");
-                                choix = s.nextInt();
-                                break;
-                        }
+                            System.out.println("Error : Merci de Saisir un chiffre soit 1 ou 2");
+                            choix = s.nextInt();
+                            break;
+                    }
                     break;
 
                 case 3:
-                    Menu.MenuGarage();
+                    f.MenuGarage();
                     choix = s.nextInt();
-                        switch (choix) {
-                            case 1:
-                                myGarage.OuvrirFenetre();
-                                break;
+                    switch (choix) {
+                        case 1:
+                            myGarage.OuvrirFenetre();
+                            break;
 
-                            case 2:
-                                myGarage.opendoor();
-                                break;
+                        case 2:
+                            myGarage.opendoor();
+                            break;
 
-                            case 3:
-                                myGarage.lookdoor();
-                                break;
+                        case 3:
+                            myGarage.lookdoor();
+                            break;
 
-                            default:
-                                System.out.println("Error : Merci de Saisir un chiffre soit 1, 2 et 3");
-                                choix = s.nextInt();
-                                break;
+                        default:
+                            System.out.println("Error : Merci de Saisir un chiffre soit 1, 2 et 3");
+                            choix = s.nextInt();
+                            break;
                     }
                     break;
 
--- projet/menu/Menu.java
+++ projet/menu/Menu.java
@@ -1,41 +1,17 @@
 package projet.menu;
 
-public class Menu {
+abstract class Menu {
 
     // Methode static pour afficher Menu
-    public static void AfficherMenu() {
-        System.out.println();
-        System.out.println("------------ Systeme d'automatisation domotique ---------------");
-        System.out.println("1 - Gestion Bedroom");
-        System.out.println("2 - Gestion Cuisine");
-        System.out.println("3 - Gestion Garage");
-        System.out.println("4 - Quitter le programme");
-        System.out.print("Veuillez saisir votre choix SVP : ");
-    }
+    public abstract void AfficherMenu(); 
 
     // Methode static pour lire les donnees
-    public static void MenuBedroom() {
-        System.out.println();
-        System.out.println("a - Allumer les lumieres");
-        System.out.println("b - Changer Etat Climatiseur");
-        System.out.print("Veuillez saisir votre choix SVP : ");
-    }
+    public abstract void MenuBedroom();
 
     // Methode static pour supprimer les donnees
-    public static void MenuCuisine() {
-        System.out.println();
-        System.out.println("a - Allumer le four");
-        System.out.println("b - Fermer la porte");
-        System.out.print("Veuillez saisir votre choix SVP : ");
-    }
+    public abstract void MenuCuisine(); 
 
     // Methode static pour supprimer les donnees
-    public static void MenuGarage() {
-        System.out.println();
-        System.out.println("a - Ouvrir les fenetres ");
-        System.out.println("b - Ouvrir la Porte");
-        System.out.println("c - Verroullier la Porte");
-        System.out.print("Veuillez saisir votre choix SVP : ");
-    }
+    public abstract void MenuGarage();
 
 }
